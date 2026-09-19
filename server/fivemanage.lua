85aafe8a5ff23022dc21175e76fa2e7a2b1ba022
OldTymeGamer <bosbornjr@gmail.com>
2026-09-18 20:48:38 -0400

    V2 Updates

--- server/fivemanage.lua
+++
@@ -1,6 +0,0 @@
-local function configured()
-    return GetConvar('otg_fivemanage_upload_url', '') ~= '' and GetConvar('otg_fivemanage_token', '') ~= ''
-end
-lib.callback.register('otg-saloon:server:imageProviderStatus', function(source)
-    return { fivemanage = configured() }
-end)
